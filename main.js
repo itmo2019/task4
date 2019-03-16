38af367a3eef943e75b617bc176a88ce32cbaab2
Denis <dr.denisv2016@yandex.ru>
2019-03-16 23:51:17 +0300

    add animation for adding and deleting messages

---
+++ main.js
@@ -0,0 +1,31 @@
+clear = function(deletedMessages) {
+    deletedMessages.forEach(
+    function(currentValue, currentIndex, listObj) {
+        currentValue.remove();
+    }); 
+}
+
+var deleteAction = document.getElementsByClassName("mail-box__delete")[0];
+deleteAction.addEventListener("click", 
+    function() {
+        var selectedMessages = document.querySelectorAll(".message-snippet__message-tick:checked");
+        selectedMessages.forEach(
+            function(currentValue, currentIndex, listObj) {
+                currentValue.parentNode.classList.add("messages-list__message-snippet_deleted");
+            });
+        var deletedMessages = document.querySelectorAll(".messages-list__message-snippet_deleted");
+        setInterval(clear, 500, deletedMessages);
+    }
+);
+
+var messageSnippet = document.getElementsByClassName("messages-list__message-snippet")[2].cloneNode(true);
+
+var getMessageAction = document.getElementsByClassName("mail-box__get-message")[0];
+getMessageAction.addEventListener("click",
+    function() {
+        var messagesList = document.getElementsByClassName('messages-list')[0];
+        var newMessage = messageSnippet.cloneNode(true);
+        messagesList.insertBefore(newMessage, messagesList.firstChild);
+        newMessage.classList.add("message_snippet_added");
+    }
+);
