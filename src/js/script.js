d7bec8ccb14dac5dbffbd45af54d67e5e218afe6
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-03-23 12:39:41 +0300

    Add lorem ipsum generation

--- src/js/script.js
+++ src/js/script.js
@@ -26,7 +26,7 @@ function minutesToMillis(minutes) {
 }
 
 function getRandomFromRange(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.round(Math.random() * (max - min) + min);
 }
 
 function newMessagePerRandomTime() {
@@ -36,9 +36,10 @@ function newMessagePerRandomTime() {
   setTimeout(newMessagePerRandomTime, timeout)
 }
 
-function showMessage(content) {
+function showMessage(message) {
   letter.style.display = "block";
   checkboxAll.disabled = true;
+  letter_content.innerText = message;
 }
 
 function closeMessage() {
@@ -49,16 +50,16 @@ function closeMessage() {
 function Message(id) {
   let element = document.createElement("div");
   this.id = id;
-
+  this.body = generateMessage();
   element.classList.add("inbox__message");
   element.id = "message_" + id.toString();
   element.appendChild(getCheckboxDiv(id));
   element.appendChild(getIconImg());
-  element.appendChild(getAuthorSpan(id));
+  element.appendChild(getAuthorSpan());
   element.appendChild(getReadDiv(id));
-  element.appendChild(getBody());
+  element.appendChild(getBody(this.body));
   element.appendChild(getDateTime());
-  element.appendChild(getLink(id));
+  element.appendChild(getLink(this.body));
   let messagesDiv = document.getElementById("messages");
   addMessageWithAnimation(messagesDiv, element);
 
@@ -74,11 +75,11 @@ function Message(id) {
     return document.getElementById("message_" + this.id.toString())
   };
 
-  function getLink(id) {
+  function getLink(body) {
     let link = document.createElement("a");
     link.classList.add("inbox__message-open-link");
     link.onclick = function () {
-      showMessage(id);
+      showMessage(body);
     };
     return link
   }
@@ -121,11 +122,11 @@ function Message(id) {
     return messageCheckbox
   }
 
-  function getAuthorSpan(id) {
+  function getAuthorSpan() {
     let body = document.createElement("span");
     body.classList.add("inbox__message-author");
     body.classList.add("inbox__message_bold");
-    body.innerText = "Котик" + id.toString() + " из Яндекса";
+    body.innerText = "Котик #" + getRandomFromRange(1, 1000) + " из Яндекса";
 
     return body
   }
@@ -138,13 +139,17 @@ function Message(id) {
     return read
   }
 
-  function getBody() {
+  function getBody(text) {
     let body = document.createElement("div");
     body.classList.add("inbox__message-body");
     body.classList.add("inbox__message_bold");
-    body.innerText = "Ну посмотрим что тут у нас";
+    body.innerText = text;
 
     return body
+  }
+
+  function generateMessage() {
+    return new LoremIpsum().paragraph(20, 60);
   }
 }
 
@@ -239,4 +244,134 @@ function removeChecked() {
   for (let i = messages.size - 1; i >= Math.max(0, messages.size - maxMainPageMessagesCount); i--) {
     messagesAsArray[i].getElement().style.display = "block"
   }
+}
+
+// was taken from https://github.com/jsilvermist/lorem-ipsum-js
+class LoremIpsum {
+
+  constructor() {
+    /**
+     * Possible words
+     *
+     * @type {Array}
+     */
+    this._words = [ 'a', 'ac', 'accumsan', 'ad', 'adipiscing', 'aenean', 'aenean', 'aliquam', 'aliquam', 'aliquet', 'amet', 'ante', 'aptent', 'arcu', 'at', 'auctor', 'augue', 'bibendum', 'blandit', 'class', 'commodo', 'condimentum', 'congue', 'consectetur', 'consequat', 'conubia', 'convallis', 'cras', 'cubilia', 'curabitur', 'curabitur', 'curae', 'cursus', 'dapibus', 'diam', 'dictum', 'dictumst', 'dolor', 'donec', 'donec', 'dui', 'duis', 'egestas', 'eget', 'eleifend', 'elementum', 'elit', 'enim', 'erat', 'eros', 'est', 'et', 'etiam', 'etiam', 'eu', 'euismod', 'facilisis', 'fames', 'faucibus', 'felis', 'fermentum', 'feugiat', 'fringilla', 'fusce', 'gravida', 'habitant', 'habitasse', 'hac', 'hendrerit', 'himenaeos', 'iaculis', 'id', 'imperdiet', 'in', 'inceptos', 'integer', 'interdum', 'ipsum', 'justo', 'lacinia', 'lacus', 'laoreet', 'lectus', 'leo', 'libero', 'ligula', 'litora', 'lobortis', 'lorem', 'luctus', 'maecenas', 'magna', 'malesuada', 'massa', 'mattis', 'mauris', 'metus', 'mi', 'molestie', 'mollis', 'morbi', 'nam', 'nec', 'neque', 'netus', 'nibh', 'nisi', 'nisl', 'non', 'nostra', 'nulla', 'nullam', 'nunc', 'odio', 'orci', 'ornare', 'pellentesque', 'per', 'pharetra', 'phasellus', 'placerat', 'platea', 'porta', 'porttitor', 'posuere', 'potenti', 'praesent', 'pretium', 'primis', 'proin', 'pulvinar', 'purus', 'quam', 'quis', 'quisque', 'quisque', 'rhoncus', 'risus', 'rutrum', 'sagittis', 'sapien', 'scelerisque', 'sed', 'sem', 'semper', 'senectus', 'sit', 'sociosqu', 'sodales', 'sollicitudin', 'suscipit', 'suspendisse', 'taciti', 'tellus', 'tempor', 'tempus', 'tincidunt', 'torquent', 'tortor', 'tristique', 'turpis', 'ullamcorper', 'ultrices', 'ultricies', 'urna', 'ut', 'ut', 'varius', 'vehicula', 'vel', 'velit', 'venenatis', 'vestibulum', 'vitae', 'vivamus', 'viverra', 'volutpat', 'vulputate' ];
+  }
+
+  /**
+   * Get random number
+   *
+   * @param  {Number} x
+   * @param  {Number} y
+   * @return {Number}
+   */
+  _random(x, y) {
+    const rnd = (Math.random() * 2 - 1) + (Math.random() * 2 - 1) + (Math.random() * 2 - 1);
+    return Math.round(Math.abs(rnd) * x + y);
+  }
+
+  /**
+   * Get random number between min and max
+   *
+   * @param  {Number} min (optional) lower result limit
+   * @param  {Number} max (optional) upper result limit
+   * @return {Number}     random number
+   */
+  _count(min, max) {
+    let result;
+    if (min && max) result = Math.floor(Math.random() * (max - min + 1) + min);
+    else if (min) result = min;
+    else if (max) result = max;
+    else result = this._random(8, 2);
+
+    return result;
+  }
+
+  /**
+   * Get random words
+   *
+   * @param  {Number} min (optional) minimal words count
+   * @param  {Number} max (optional) maximal words count
+   * @return {Object}     array of random words
+   */
+  words(min, max) {
+    const result = [];
+    const count = this._count(min, max);
+
+    // get random words
+    while (result.length < count) {
+      const pos = Math.floor(Math.random() * this._words.length);
+      const rnd = this._words[pos];
+
+      // do not allow same word twice in a row
+      if (result.length && result[result.length - 1] === rnd) {
+        continue;
+      }
+
+      result.push(rnd);
+    }
+
+    return result;
+  }
+
+  /**
+   * Generate sentence
+   *
+   * @param  {Number} min (optional) minimal words count
+   * @param  {Number} max (optional) maximal words count
+   * @return {String}     sentence
+   */
+  sentence(min, max) {
+    const words = this.words(min, max);
+
+    // add comma(s) to sentence
+    let index = this._random(6, 2);
+    while (index < words.length - 2) {
+      words[index] += ',';
+      index += this._random(6, 2);
+    }
+
+    // append puctation on end
+    const punct = '...!?'
+    words[words.length - 1] += punct.charAt(Math.floor(Math.random() * punct.length));
+
+    // uppercase first letter
+    words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1);
+
+    return words.join(' ');
+  }
+
+  /**
+   * Generate paragraph
+   *
+   * @param  {Number} min (optional) minimal words count
+   * @param  {Number} max (optional) maximal words count
+   * @return {String}     paragraph
+   */
+  paragraph(min, max) {
+    if (!min && !max) {
+      min = 20;
+      max = 60;
+    }
+
+    const count = this._count(min, max);
+    let result = '';
+
+    // append sentences until limit is reached
+    while (result.slice(0, -1).split(' ').length < count) {
+      result += this.sentence() + ' ';
+    }
+    result = result.slice(0, -1);
+
+    // remove words
+    if (result.split(' ').length > count) {
+      const punct = result.slice(-1);
+      result = result.split(' ').slice(0, count).join(' ');
+      result = result.replace(/,$/, '');
+      result += punct;
+    }
+
+    return result;
+  }
+
 }
