c849a5fdb87b57d019e4178b9c024d985ffb4830
Anton Kopitsa <anton17.04.1998@gmail.com>
2019-03-23 19:33:07 +0300

    Add - message generation

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let messageTemplate = '      <div class="message-info__left-part">\n' +
-    '                        <input type="checkbox" class="select-message-checkbox checkbox" onclick="selectCheckbox(this)">\n' +
-    '                        <div class="message-info__sender-logo">Я</div>\n' +
-    '                        <div class="message-info__sender bold">Команда Яндекс.Почты</div>\n' +
-    '                        <div class="message-info__mark unread-mark"></div>\n' +
-    '                    </div>\n' +
-    '                    <div class="message-info__subject bold">Как читать почту с мобильного</div>\n' +
-    '                    <div class="message-info__date-container">\n' +
-    '                        <div class="date-container__date">6 июл</div>\n' +
-    '                    </div>';
+let senders = ["Петя", "Вася", "Маша"];
+let subjects = ["Привет из России", "Hello from England", "Bonjour de France"];
+let texts = ["Привет!", "Hello!", "Bonjour!"];
+let months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
 
 let anyCheckboxIsActive = false;
+let idToHtmlMap = new Map();
+
+window.onload = function () {
+    createMessage();
+};
 
 function selectCheckbox(checkbox) {
     if (checkbox.checked) {
@@ -49,8 +48,7 @@ function selectAll(selectAllCheckbox) {
 function createMessage() {
     let messagesList = document.getElementsByClassName('messages-list')[0];
     let newMessage = document.createElement('div');
-    newMessage.className = 'message';
-    newMessage.innerHTML = messageTemplate;
+    buildNewMessage(newMessage);
     newMessage.style.height = '0';
     messagesList.insertBefore(newMessage, messagesList.children[0]);
 
@@ -90,4 +88,97 @@ function deleteSelectedMessages() {
     document.getElementById('check-all').checked = false;
     let deleteButton = document.getElementById('delete-messages');
     deleteButton.style.cursor = 'text';
+}
+
+function openMessage(message) {
+    let hiddenMessage = document.getElementsByClassName('hidden-message')[0];
+    let hiddenMessageContent = document.getElementsByClassName('hidden-message__content')[0];
+    let messagesList = document.getElementsByClassName('messages-list')[0];
+
+    let hiddenHtml = idToHtmlMap.get(message.id);
+
+    hiddenMessageContent.innerHTML = '';
+    hiddenMessageContent.appendChild(hiddenHtml);
+
+    messagesList.style.display = "none";
+    hiddenMessage.style.display = "block";
+}
+
+function closeMessage() {
+    let hiddenMessage = document.getElementsByClassName('hidden-message')[0];
+    let messagesList = document.getElementsByClassName('messages-list')[0];
+
+    hiddenMessage.style.display = "none";
+    messagesList.style.display = "block";
+}
+
+function buildNewMessage(newMessage) {
+    let currentDate = new Date();
+
+    let id = currentDate.getTime();
+    let hiddenHtml = document.createElement('div');
+    hiddenHtml.classList.add('hidden-message__text');
+    let langInd = Math.floor(Math.random() * senders.length);
+    hiddenHtml.textContent = texts[langInd];
+
+    idToHtmlMap.set(id.toString(), hiddenHtml);
+
+    newMessage.id = id;
+    newMessage.classList.add('message');
+
+    let leftPart = document.createElement('div');
+    leftPart.classList.add('message-info__left-part');
+
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('select-message-checkbox');
+    checkbox.classList.add('checkbox');
+    checkbox.onclick = function () {
+        selectCheckbox(this);
+    };
+    leftPart.appendChild(checkbox);
+
+    let senderName = senders[Math.floor(Math.random() * senders.length)];
+
+    let senderLogo = document.createElement('div');
+    senderLogo.classList.add('message-info__sender-logo');
+    senderLogo.textContent = senderName[0];
+    leftPart.appendChild(senderLogo);
+
+    let sender = document.createElement('div');
+    sender.classList.add('message-info__sender');
+    sender.classList.add('bold');
+    sender.textContent = senderName;
+    leftPart.appendChild(sender);
+
+    let unreadMark = document.createElement('div');
+    unreadMark.classList.add('message-info__mark');
+    unreadMark.classList.add('unread-mark');
+    leftPart.appendChild(unreadMark);
+
+    newMessage.appendChild(leftPart);
+
+    let subject = document.createElement('div');
+    subject.classList.add('message-info__subject');
+    subject.classList.add('bold');
+    subject.textContent = subjects[langInd];
+
+    newMessage.appendChild(subject);
+
+    let dateContainer = document.createElement('div');
+    dateContainer.classList.add('message-info__date-container');
+
+    let monthInd = currentDate.getMonth().toLocaleString('rus');
+    month = months[monthInd];
+    let day = currentDate.getDate();
+
+    let date = document.createElement('div');
+    date.classList.add('date-container__date');
+    date.textContent = day + ' ' + month.substr(0,3);
+
+    dateContainer.appendChild(date);
+    newMessage.appendChild(dateContainer);
+    newMessage.onclick = function () {
+        openMessage(this);
+    };
 }
