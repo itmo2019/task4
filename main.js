aa7d2a26671c87b26a305059c49d8ee9b7776972
Denis <dr.denisv2016@yandex.ru>
2019-03-29 00:12:50 +0300

    review fixes

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ clear = function(deletedMessages) {
     });
 }
 
-var deleteAction = document.getElementsByClassName("mail-box__delete")[0];
-deleteAction.addEventListener("click",
+setActionHandler = function(className, listener) {
+    var action = document.querySelector("." + className);
+    action.addEventListener("click", listener);
+}
+
+var messageSnippet = document.getElementsByClassName("messages-list__message-snippet")[2].cloneNode(true);
+
+setActionHandler("mail-box__delete", 
     function() {
         var selectedMessages = document.querySelectorAll(".message-snippet__message-tick:checked");
         selectedMessages.forEach(
@@ -18,14 +24,15 @@ deleteAction.addEventListener("click",
     }
 );
 
-var messageSnippet = document.getElementsByClassName("messages-list__message-snippet")[2].cloneNode(true);
-
-var getMessageAction = document.getElementsByClassName("mail-box__get-message")[0];
-getMessageAction.addEventListener("click",
+setActionHandler("mail-box__get-message",
     function() {
-        var messagesList = document.getElementsByClassName('messages-list')[0];
+        var messagesList = document.querySelector('.messages-list');
         var newMessage = messageSnippet.cloneNode(true);
+        newMessage.classList.add("message_snippet_start_adding");
         messagesList.insertBefore(newMessage, messagesList.firstChild);
-        newMessage.classList.add("message_snippet_added");
+        var triggerAnimation = function() {
+            newMessage.classList.add("message_snippet_added");
+        };  
+        setInterval(triggerAnimation, 0);
     }
 );
