d66e62629b2b9588dcff1c8cfc889376f894e613
Drozdov Gleb <drozdov.gleb.spb@gmail.com>
2019-03-31 02:14:09 +0300

    fix remarks

--- scripts/letter-worker.js
+++ scripts/letter-worker.js
@@ -2,40 +2,66 @@ let messagesContainer = document.getElementsByClassName('messages-box');
 let messagesBox = document.getElementsByClassName('messages-box')[0];
 let hiddenBox = document.getElementsByClassName('hidden-box')[0];
 let hiddenBoxContent = document.getElementsByClassName('hidden-box__content')[0];
-let localStorage = new Map();
+let localMessagesStorage = new Map();
+let maxMessagePerPage = 30;
+let timeMessageInterval = 5 * 60 * 1000;
+let maxMessageInterval = 10 * 60 * 1000;
+let wasNormalInterval = true;
 
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-setTimeout(newMail, getRandomArbitrary(10, 10 * 60 * 1000));
+setTimeout(newMail, getRandomArbitrary(10, maxMessageInterval));
 
-function newMail() {
+function getTimeForMessage() {
+    let randomTime = getRandomArbitrary(10, maxMessageInterval);
+    if (wasNormalInterval) {
+        if (randomTime < timeMessageInterval) {
+            wasNormalInterval = false;
+        }
+    } else {
+        randomTime = getRandomArbitrary(timeMessageInterval, maxMessageInterval)
+        wasNormalInterval = true;
+    }
+    console.log(randomTime / 1000);
+    console.log(new Date().getTime() / 1000);
+    return randomTime;
+}
+
+async function newMail() {
+    let timeForMessage = getTimeForMessage();
     let newGeneratedMessage = document.createElement('div');
     newGeneratedMessage.className = 'message';
-    let newMessage = generateMessage(newGeneratedMessage);
+    let newMessage = await generateMessage(newGeneratedMessage);
     newMessage.onclick = function () {
         openMessage(this, event);
     };
+    newMessage.classList.add("add-message-animation");
     messagesContainer[0].insertBefore(newMessage, messagesContainer[0].firstChild);
-    let animationDuration = 1500;
-    let fps = animationDuration / 30;
-    animate(newMessage, fps, animationDuration, false);
-    setTimeout(newMail, getRandomArbitrary(5 * 60 * 1000, 10 * 60 * 1000))
+    newMessage.addEventListener("animationend", () => {
+        newMessage.classList.remove("add-message-animation");
+    });
+    setTimeout(newMail, timeForMessage);
 }
 
-function generateMessage(message) {
+async function generateMessage(message) {
+    if (localMessagesStorage.size >= maxMessagePerPage) {
+        let lastMessageOnPageId = Array.from(localMessagesStorage.keys())[localMessagesStorage.size - maxMessagePerPage];
+        document.getElementById(lastMessageOnPageId).style.display = "none";
+    }
     let id = new Date().getTime();
     message.id = id;
 
-    [theme, text] = getRandomThemeAndText();
-    localStorage.set(id.toString(), text);
+    let senderName = getRandomSender();
+    let [theme, text] = await getRandomThemeAndText();
+    localMessagesStorage.set(id.toString(), text);
 
     message.appendChild(generateInput());
     message.appendChild(document.createTextNode("\n"));
-    message.appendChild(generateSenderLogo());
+    message.appendChild(generateSenderLogo(senderName[0]));
     message.appendChild(document.createTextNode("\n"));
-    message.appendChild(generateSenderDiv());
+    message.appendChild(generateSenderDiv(senderName));
     message.appendChild(document.createTextNode("\n"));
     message.appendChild(generateUnreadCircle());
     message.appendChild(document.createTextNode("\n"));
@@ -43,15 +69,13 @@ function generateMessage(message) {
     message.appendChild(document.createTextNode("\n"));
     message.appendChild(generateDateDiv());
 
-    console.log(message.outerHTML);
-
     return message;
 }
 
-function addMessage() {
+async function addMessage() {
     let newMessage = document.createElement('div');
     newMessage.className = 'message';
-    newMessage = generateMessage(newMessage);
+    newMessage = await generateMessage(newMessage);
     newMessage.onclick = function () {
         openMessage(this, event);
     };
@@ -63,19 +87,20 @@ function addMessage() {
 }
 
 function removeElement(el) {
+    let removedElementId = el.id;
     el.classList.add("delete-message-animation");
     el.addEventListener("animationend", () => {
         el.remove();
     });
+    localMessagesStorage.delete(removedElementId);
 }
 
 function openMessage(messageOuter, messageInner) {
-    let message = localStorage.get(messageOuter.id);
+    let message = localMessagesStorage.get(messageOuter.id);
     if (messageInner.target.className !== 'message__checkbox') {
         hiddenBoxContent.innerHTML = message;
         messagesBox.style.display = "none";
         hiddenBox.style.display = "block";
-        console.log(message);
     }
 }
 
@@ -88,7 +113,6 @@ function generateDateDiv() {
     let today = new Date();
     let options = {month: 'long', day: 'numeric'};
     let formattedDate = today.toLocaleDateString("ru-RU", options);
-    console.log(formattedDate);
     let date = document.createElement('div');
     date.className = 'message__date';
     date.innerText = formattedDate;
@@ -102,17 +126,18 @@ function generateThemeDiv(theme) {
     return themeDiv;
 }
 
-function generateSenderDiv() {
+function generateSenderDiv(senderName) {
     let senderDiv = document.createElement('div');
     senderDiv.className = 'message__sender';
-    senderDiv.innerText = getRandomSender() + " ";
+    senderDiv.innerText = senderName + " ";
     return senderDiv;
 }
 
-function generateSenderLogo() {
-    let senderLogoDiv = document.createElement('img');
-    senderLogoDiv.className = 'message__ya-img';
-    senderLogoDiv.src = 'resources/ya-default.png';
+function generateSenderLogo(firstSenderChar) {
+    let senderLogoDiv = document.createElement('div');
+    senderLogoDiv.classList.add('sender-img');
+    senderLogoDiv.classList.add('message__sender-img');
+    senderLogoDiv.textContent = firstSenderChar;
     return senderLogoDiv;
 }
 
@@ -135,14 +160,25 @@ function deleteMessages() {
     for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             let message = checkboxes[i].parentElement;
-            removeElement(message);
+            if (message.style.display !== "none") {
+                removeElement(message);
+            }
         }
+    }
+    document.querySelector('#select-all-checkbox').checked = false;
+    showHiddenMessages();
+}
+
+function showHiddenMessages() {
+    let keysArray = Array.from(localMessagesStorage.keys());
+    let storageSize = localMessagesStorage.size;
+    for (let i = Math.max(0, storageSize - maxMessagePerPage); i < storageSize; i++) {
+        document.getElementById(keysArray[i]).style.display = "block";
     }
 }
 
 function selectAllMessages() {
     let isCheckedAll = document.querySelector('#select-all-checkbox').checked;
-    console.log(isCheckedAll);
     let checkboxes = document.body.querySelectorAll('.message__checkbox');
     for (let i = 0; i < checkboxes.length; i++) {
         console.log(checkboxes[i].id);
--- scripts/message-templates.js
+++ scripts/message-templates.js
@@ -1,20 +1,15 @@
 let senders = ['Championat.com', 'Sportbox', 'Матч ТВ', 'Eurosport'];
 
-let themes = ['Победа сборной России над cборной Казахстана', 'Никита Кучеров бьёт рекорды', 'Ставки на матч Голландия-Германия', 'Джеймс Харден всё ближе к MVP'];
-
-let texts = ['Сборная Россия одеражала победу со счётом 0-4 над сборной Казахстан',
-    'Нападающий клуба НХЛ Таймпа-Бэй близок уже набрал 120 очков в регулярном чемпионате и близок к рекорду Могильного(127 очков)',
-    'Букмекеры отдают небольшое предпочтение хозяевам поля, коэффициент на победу которых равен 2,40, тогда как успех гостей оценивается в 2,75, а ничья — в 3,30.',
-    'Игрок клуба Хьюстон Рокетс уже второй раз в сезоне набрал 61 очко и продолжает лидировать в списке снайперов НБА'];
-
-
 function getRandomSender() {
     return senders[getRandomIndex(senders.length)];
 }
 
-function getRandomThemeAndText() {
-    let index = getRandomIndex(themes.length);
-    return [themes[index], texts[index]];
+async function getRandomThemeAndText() {
+    let responseText = await fetch("https://baconipsum.com/api/?type=meat-and-filler");
+    let responseTheme = await fetch("https://baconipsum.com/api/?type=meat-and-filler&sentences=1");
+    let dataText = await responseText.json();
+    let dataTheme = await responseTheme.json();
+    return [dataTheme[0], dataText[0]];
 }
 
 function getRandomIndex(arraySize) {
