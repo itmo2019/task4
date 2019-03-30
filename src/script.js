f2bbfb227b3a5a8a825fe725611c0b439125e89c
Maxim <m.a.tereshchuk@gmail.com>
2019-03-30 15:21:53 +0300

    Remarks from task3 fixed

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 let cnt = 2;
-let letterTemplate = '<input type="checkbox" class="letter__check">'
-    + '<img class="letter__sender-logo" src="../images/yandex-logo.png">'
-    + '<div class="letter__sender-name letter__sender-name_unread">Преподы по фронту</div>'
-    + '<div class="letter__mark letter__mark_unread"></div>'
-    + '<div class="letter__title letter__title_unread"></div>'
-    + '<div class="letter__date">6 июл</div>';
+let letterTemplate = `<input type="checkbox" class="letter__check">
+    <img class="letter__sender-logo" src="../images/yandex-logo.png">
+    <div class="letter__sender-name letter__sender-name_unread">Преподы по фронту</div>
+    <div class="letter__mark letter__mark_unread"></div>
+    <div class="letter__title letter__title_unread"></div>
+    <div class="letter__date">6 июл</div>`;
 
 // document.getElementById("menu__receive").addEventListener("click", receiveLetter);
 document.getElementById("actions__remove").addEventListener("click", remove);
@@ -34,7 +34,7 @@ function remove() {
     for (let i = checkboxs.length - 1; i >= 0; i--) {
         if (checkboxs[i].checked) {
             let letter = checkboxs[i].parentNode;
-            letter.classList.add("will_be_removed");
+            letter.classList.add("letter_will-be-removed");
             letter.addEventListener("animationend", (ev) => ev.target.remove());
         }
     }
@@ -47,4 +47,4 @@ function checkAll() {
     }
 }
 
-setInterval(receiveLetter, nextDouble(1000, 10000));
+setInterval(receiveLetter, nextDouble(2000, 2000));
