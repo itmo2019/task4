7b239d052e941680552c0abf779cc1747aff5f60
alexust27 <ustinov1998s@mail.ru>
2019-03-31 18:51:26 +0300

    done 4 task

--- js/actions.js
+++ js/actions.js
@@ -1,48 +1,186 @@
-let oneLetter = '<label>\n' +
-    '                    <input type="checkbox" class="check">\n' +
-    '                </label>\n' +
-    '                <div class="main-block__img-ya">\n' +
-    '                    <img src="img/yandex.png" alt="Yandex">\n' +
-    '                </div>\n' +
-    '                <span class="main-block__mail-from bold-text">Яндекс</span>\n' +
-    '                <div class="main-block__mail-not-read"></div>\n' +
-    '                <span class="main-block__topic bold-text">Собери свою поту в этот ящик</span>\n' +
-    '                <time class="main-block__date" datetime="06.07">6 июл</time>';
+const LETTERS_ON_PAGE = 30;
+
+let count = 0;
+let companies = ['ebay', 'facebook', 'live', 'yandex', 'live', 'reddit', 'twitter', 'youtube'];
+let topic = ["Внимание!", "Добро пожаловать!", "Обновление", "Получите приз!", "Восстановление аккаунта",
+    "Ищем сотрудников", "Спасибо за отзыв"];
+let hello = ["Здравствуйте!", "Добрый день!", "Привет!", "Приветствую!", "Салют!"];
+let word1 = ["Я", "Меня зовут", "Это"];
+let firstName = ["Виталий", "Андрей", "Владимир", "Алексей", "Артём", "Антон"];
+let secondName = ["Соболев", "Чиркин", "Борисов", "Орехов", "Гаврилов", "Иванов", "Сергеев", "Онегин"];
+let phrase1 = ["Так вышло, что", "Нам стало известно, что", "Сообщаем вам, что",
+    "Как вы могли заметить,", "С сегоднящнего дня"];
+let nouns1 = ["эксперт", "редактор", "программист", "рабочий"];
+let verbs = ["взломал", "проверил", "удалил", "исправил", "закрыл", "заметил",
+    "пометил", "пересмотрел", "передал"];
+let nouns2 = ["счет", "аккаунт", "пароль", "кабинет"];
+let adjectives = ["идеальный", "прямой", "обратный", "наш",
+    "постоянный", "великолепный", "исключительный", "личный", "ваш"];
+let isOpen = false;
+
+function randomInt(min, max) {
+    return Math.floor((max - min) * Math.random() + min);
+}
+
+function randFromList(list) {
+    return list[randomInt(0, list.length)];
+}
+
+function randomDate(date) {
+    let m = randomInt(1, 12);
+    let day = randomInt(1, 30);
+    if (m === 2) day = Math.min(28, day);
+    let month = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
+    date.dateTime = day + '.' + m;
+    date.textContent = String(day) + ' ' + String(month[m - 1]);
+}
+
+
+function generateText(letter, sender) {
+    letter.querySelector("p").textContent =
+        randFromList(hello)
+        + ' ' + randFromList(word1)
+        + ' ' + randFromList(firstName)
+        + ' ' + randFromList(secondName)
+        + ', глава компании ' + sender.toUpperCase()
+        + '.'
+    ;
+    letter.querySelectorAll("p")[1].textContent =
+        randFromList(phrase1)
+        + ' ' + randFromList(adjectives)
+        + ' ' + randFromList(nouns1)
+        + ' ' + randFromList(verbs)
+        + ' ' + randFromList(adjectives)
+        + ' ' + randFromList(nouns2) + '.'
+    ;
+
+}
+
+function createLetter() {
+    let letter = document.createElement('li');
+    letter.className = 'main-block__letter animation-insert';
+    letter.setAttribute('data-method-click', 'clicked');
+    let clone = document.getElementById("template-letter").content.cloneNode(true);
+    let sender = randFromList(companies);
+    clone.querySelector("img").src = 'img/icons/' + sender + '.png';
+    clone.querySelector('.main-block__mail-from').textContent = sender.toUpperCase();
+    clone.querySelector('.main-block__topic').textContent = randFromList(topic);
+    randomDate(clone.querySelector("time"));
+    generateText(clone.querySelector(".main-block__letter-content"), sender);
+    letter.append(clone);
+    return letter;
+}
+
+function hideLetters(letters) {
+    for (let i = LETTERS_ON_PAGE - 1; i < letters.length; i++) {
+        letters[i].style.display = 'none';
+        letters[i].querySelector('.check').checked = false;
+    }
+}
 
 function addLetter() {
+    count++;
+    console.log('add:' + count);
+    document.querySelector('.check').checked = false;
     let allLetters = document.getElementById('all-letters');
-    let newLetter = document.createElement('div');
-    newLetter.className = 'animation-insert main-block__letter';
-    newLetter.innerHTML = oneLetter;
-    allLetters.insertBefore(newLetter, allLetters.childNodes[2]);
-    allLetters.classList.add('all-letter-down');
-    setTimeout(removeClass, 600, newLetter, 'animation-insert');
-    setTimeout(removeClass, 600, allLetters, 'all-letter-down');
-
+    if (count > LETTERS_ON_PAGE) {
+        hideLetters(allLetters.querySelectorAll("li"));
+    }
+    let newLetter = createLetter();
+    allLetters.insertBefore(newLetter, allLetters.querySelectorAll("li")[0]);
+    if (!isOpen)
+        allLetters.classList.add('all-letter-down');
+    newLetter.addEventListener("webkitAnimationEnd", function () {
+        removeClass(newLetter, 'animation-insert');
+    });
+    allLetters.addEventListener("webkitAnimationEnd", function () {
+        removeClass(allLetters, 'all-letter-down');
+    });
 }
 
 function removeClass(letter, name) {
     letter.classList.remove(name);
 }
 
-function removeWithAnimation(letter) {
-    removeClass(letter, 'animation-delete');
-    letter.remove();
-}
-
-function remove() {
-    let checkboxes = document.body.querySelectorAll('.check');
-    for (let i = 1; i < checkboxes.length; i++) {
-        if (checkboxes[i].checked) {
-            let letter = checkboxes[i];
-            while (!letter.classList.contains('main-block__letter')) {
-                letter = letter.parentElement;
-            }
-            letter.classList.add('animation-delete');
-            setTimeout(removeWithAnimation, 600, letter);
-        }
+function checkVisibility() {
+    let letters = document.getElementById('all-letters').querySelectorAll("li");
+    for (let i = 0; i < Math.min(letters.length, LETTERS_ON_PAGE); i++) {
+        letters[i].style.display = 'block';
     }
 }
 
-document.getElementById('get-letter').addEventListener("click", addLetter);
-document.getElementById('remove').addEventListener("click", remove);
+function removeLetters() {
+    if (isOpen) return;
+    console.log('remove:' + count);
+    document.body.querySelector('.check').checked = false;
+    let letters = document.getElementById('all-letters').querySelectorAll("li");
+    letters.forEach(letter => {
+        if (letter.querySelector(".check").checked) {
+            letter.classList.add('animation-delete');
+            letter.addEventListener("webkitAnimationEnd", function () {
+                removeClass(letter, 'animation-delete');
+                count--;
+                letter.remove();
+                checkVisibility();
+            });
+        }
+    });
+}
+
+function selectAll() {
+    if (isOpen) return;
+    let checkboxes = document.body.querySelectorAll('.check');
+    let isChecked = checkboxes[0].checked;
+    for (let i = 1; i < Math.min(checkboxes.length, LETTERS_ON_PAGE + 1); i++) {
+        checkboxes[i].checked = isChecked;
+    }
+}
+
+function getRandomLetter() {
+    let t = randomInt(10, 300000) + 300000;
+    addLetter();
+    console.log(t);
+    setTimeout(getRandomLetter, t);
+}
+
+function _displayNone(obj) {
+    obj.style.display = 'none';
+}
+
+function _displayBlock(obj) {
+    obj.style.display = 'block';
+}
+
+function openLetter(event) {
+    let target = event.target;
+    console.log(target);
+    if (target.tagName !== 'INPUT' && target.type !== 'checkbox'
+        && !target.classList.contains('main-block__all-letters')) {
+        if (target.dataset.closeLetter === 'close') {
+            while (!target.classList.contains('main-block__letter')) {
+                target = target.parentElement;
+            }
+            isOpen = false;
+            _displayNone(target.querySelector('.main-block__letter-content'));
+            _displayBlock(target.querySelector("div"));
+        } else { //open
+            while (!target.classList.contains('main-block__letter')) {
+                target = target.parentElement;
+            }
+            isOpen = true;
+            _displayNone(target.querySelector("div"));
+            _displayBlock(target.querySelector('.main-block__letter-content'));
+        }
+    }
+    console.log(target);
+}
+
+let Event = new CustomEvent('customAdd', getRandomLetter);
+let Elem = document.body;
+Elem.addEventListener('customAdd', getRandomLetter);
+Elem.dispatchEvent(Event);
+
+document.getElementById('all-letters').addEventListener('click', openLetter);
+document.getElementById('get-letter').addEventListener('click', addLetter);
+document.getElementById('remove').addEventListener('click', removeLetters);
+document.querySelector('.check').addEventListener('click', selectAll);
