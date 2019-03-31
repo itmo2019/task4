9ba9543fe014ba5316a3d2d3b9cb1cc0da625c0b
Maxim <m.a.tereshchuk@gmail.com>
2019-04-01 00:56:49 +0300

    Remarks fixed

--- src/script.js
+++ src/script.js
@@ -1,13 +1,14 @@
-let mailTemplate = 
-    `<input type="checkbox" class="mail__check">
-    <img class="mail__sender-logo">
-    <div class="mail__sender-name mail__sender-name_unread"></div>
-    <div class="mail__mark mail__mark_unread"></div>
-    <div class="mail__title mail__title_unread"></div>
-    <div class="mail__date"></div>`;
+const letterLineTemplate = 
+    `<input type="checkbox" class="letter-line__check">
+    <img class="letter-line__sender-logo">
+    <div class="letter-line__sender-name letter-line__sender-name_unread"></div>
+    <div class="letter-line__mark letter-line__mark_unread"></div>
+    <div class="letter-line__title letter-line__title_unread"></div>
+    <time class="letter-line__date"></time>
+    <div class="letter-line__click-area"></div>`;
 
-let senders = ['Meduza', 'ZNAK', 'Радио Свобода', 'Новая Газета', 'газета.ru', 'Известия'];
-let keyWords = ['президент', 'России', 'Владимир', 'Путин', 
+const senders = ['Meduza', 'ZNAK', 'Радио Свобода', 'Новая Газета', 'газета.ru', 'Известия'];
+const keyWords = ['президент', 'России', 'Владимир', 'Путин', 
 'подписал', 'закон', 'предусматривающий', 'блокировку', 
 'недостоверных', 'новостей', 'а', 'также', 'материалов', 
 'оскорбляющих', 'общество', 'государственные', 'символы', 
@@ -29,104 +30,155 @@ let keyWords = ['президент', 'России', 'Владимир', 'Пу�
 'стажа', 'женщинам', '37', 'лет', 'тем', 'не', 'менее', 'уровень', 
 'бедности', 'сохраняется', 'высоким', '193', 'миллион', 'человек', 'то',
 'есть', '13%', 'населения', 'имеют', 'доход', 'ниже', 'прожиточного', 'минимума'];
-let punctMarks = ['.', ',', '!', '?', ':', ';']
+const punctMarks = ['.', '.', '.', '.', ',', ',', ',', ',', '!', '?', ':', ';']
+const months = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'сен', 'окт', 'ноя', 'дек'];
+
+const imgPath = '../images/';
+const imgMaskEnd = '-logo.png';
+const minTextWordsNum = 70;
+const maxTextWordsNum = 200;
+const minDistPunctMarks = 5;
+const maxDistPunctMarks = 10;
+const letterTimeDist = 300000;
+const minNewLetterTime = 10;
+const maxNewLetterTime = 600000;
+const maxLettersNumberOnPage = 30;
+
+let curNumOfLettersOnPage = 0;
+let hiddenLetters = [];
+let shortLastLetterTimeDist = false;
 
 function randRange(lowBound, uppBound) {
-    return Math.round(Math.random() * ((uppBound - 1) - lowBound) + lowBound);
+    return Math.round(Math.random() * (uppBound - lowBound) + lowBound);
 }
 
-function randUpBound(uppBound) {
-    return randRange(0, uppBound)
+function randUpBoundEx(uppBound) {
+    return randRange(0, uppBound - 1);
 }
-
-function newMail() {
-    let mails = document.getElementById('mail-box');
-    let mail = genMail();
-    mails.insertBefore(mail, mails.firstChild);
-}
-
-let cnt = 1;
-
-function genMail() {
-    let mail = document.createElement('div');
-    mail.className = 'mail';
-    let id = 'mail_' + cnt;
-    mail.id = id;
-    mail.innerHTML = mailTemplate;
-    let sender = senders[randUpBound(senders.length)];
-    mail.querySelector('.mail__sender-logo').src = '../images/' + sender + '-logo.png';
-    mail.querySelector('.mail__sender-name').textContent += sender;
-    //mail.querySelector('.mail__text').textContent += genText();
-    genText();
-    setTimeout(function() {
-         mail.classList.add('show');
-    }, 10);
-    mail.addEventListener('click', () => help(id))
-    return mail;
-}
-
-function help(id) {
-    //document.getElementById(id).querySelector('.mail__text').style.display = 'block';
-}
- 
-let minTextWordsNum = 30;
-let maxTextWordsNum = 50;
-let minDistPunctMarks = 3;
-let maxDistPunctMarks = 7;
 
 function genText() {
-    let size = randRange(minTextWordsNum, maxTextWordsNum);
+    let wordsNum = randRange(minTextWordsNum, maxTextWordsNum);
     let text = '';
-    let cnt = 0;
-    let next = randRange(minDistPunctMarks, maxDistPunctMarks);
-    let nextIsUpperCase = true;
-    for (let i = 0; i < size; i++) {
-        let word = keyWords[randUpBound(keyWords.length)];
-        if (nextIsUpperCase) {
+    let curWordsNumNoPM = 0;
+    let wordsNumForNextPM = randRange(minDistPunctMarks, maxDistPunctMarks);
+    let nextWordIsUpperCase = true;
+    for (let i = 0; i < wordsNum; i++) {
+        let word = keyWords[randUpBoundEx(keyWords.length)];
+        if (nextWordIsUpperCase) {
             word = word[0].toUpperCase() + word.slice(1);
-            nextIsUpperCase = false;
+            nextWordIsUpperCase = false;
         }
+
         text += word;
-        cnt++;
-        if (cnt == next) {
-            let punctMark = punctMarks[randUpBound(punctMarks.length)];
-            text += punctMark;
-            cnt = 0;
-            next = randRange(minDistPunctMarks, maxDistPunctMarks);
+        curWordsNumNoPM++;
+
+        if (curWordsNumNoPM == wordsNumForNextPM && i != wordsNum - 1) {
+            let punctMark = punctMarks[randUpBoundEx(punctMarks.length)];
             if (punctMark == '.' || punctMark == '?' || punctMark == '!') {
-                nextIsUpperCase = true;
+                nextWordIsUpperCase = true;
             }
+
+            text += punctMark;
+            curWordsNumNoPM = 0;
+            wordsNumForNextPM = randRange(minDistPunctMarks, maxDistPunctMarks);
         }
-        if (i == size - 1) {
-            if (!punctMarks.some(pm => pm == text[text.length - 1])) {
-                text += '.'
-            }
-        } else {
+
+        if (i < wordsNum - 1) {
             text += ' ';
+        } else {
+            text += '.'
         }
     }
     return text;
 }
 
+function showEntryBoard(text) {
+    document.getElementById('letter-line-box').style.display = 'none';
+    document.getElementById('letter-entry-board').style.display = 'block';
+    document.getElementById('letter-entry-board__text').textContent = text;
+}
+
+function closeEntryBoard() {
+    document.getElementById('letter-line-box').style.display = 'block';
+    document.getElementById('letter-entry-board').style.display = 'none';
+}
+
+function genLetter() {
+    let text = genText();
+    let sender = senders[randUpBoundEx(senders.length)];
+    let date = new Date();
+
+    let letterLine = document.createElement('div');
+    letterLine.className = 'letter-line';
+    letterLine.innerHTML = letterLineTemplate;
+    letterLine.querySelector('.letter-line__sender-logo').src = imgPath + sender + imgMaskEnd;
+    letterLine.querySelector('.letter-line__sender-name').textContent = sender;
+    letterLine.querySelector('.letter-line__title').textContent = text;
+    letterLine.querySelector('.letter-line__date').textContent = date.getDate() + ' ' + months[date.getMonth()];
+    letterLine.querySelector('.letter-line__click-area').addEventListener('click', () => showEntryBoard(text));
+    setTimeout(function() {
+         letterLine.classList.add('show');
+    }, 10);
+    return letterLine;
+}
+
 function remove() {
-    let checkboxs = document.getElementsByClassName('mail__check');
-    for (let i = checkboxs.length - 1; i >= 0; i--) {
-        if (checkboxs[i].checked) {
-            let mail = checkboxs[i].parentNode;
-            mail.classList.add('mail_will-be-removed');
-            mail.addEventListener('animationend', (ev) => ev.target.remove());
+    let checkboxes = document.getElementsByClassName('letter-line__check');
+    for (let i = checkboxes.length - 1; i >= 0; i--) {
+        if (checkboxes[i].checked) {
+            let letterLine = checkboxes[i].parentNode;
+            letterLine.classList.add('letter-line_will-be-removed');
+            letterLine.addEventListener('animationend', (ev) => ev.target.remove());
+            curNumOfLettersOnPage--;
         }
     }
+    setTimeout(function() {
+        let letterBox = document.getElementById('letter-line-box');
+        while (hiddenLetters.length > 0 && curNumOfLettersOnPage < maxLettersNumberOnPage) {
+            letterBox.append(hiddenLetters.pop());
+            curNumOfLettersOnPage++;
+        }
+    }, 1500);
 }
 
 function checkAll() {
-    let checkboxs = document.getElementsByClassName('mail__check');
-    for (let i = 0; i < checkboxs.length; i++) {
-        checkboxs[i].checked = document.getElementById('actions__check').checked;
+    let checkStatus = document.getElementById('actions__check').checked;
+    let checkboxes = document.getElementsByClassName('letter-line__check');
+    for (let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = checkStatus;
     }
 }
 
-document.getElementById('menu__receive').addEventListener('click', newMail);
+function letterArrival() {
+    newMail();
+    let nextTimeDist;
+    if (shortLastLetterTimeDist) {
+        nextTimeDist = randRange(letterTimeDist, maxNewLetterTime);
+        shortLastLetterTimeDist = false;
+    } else {
+        nextTimeDist = randRange(minNewLetterTime, maxNewLetterTime);
+        if (nextTimeDist < letterTimeDist) {
+            shortLastLetterTimeDist = true;
+        }
+    }
+    setTimeout(letterArrival, nextTimeDist);
+}
+
+function newMail() {
+    let letter = genLetter();
+    let letterBox = document.getElementById('letter-line-box');
+    if (curNumOfLettersOnPage >= maxLettersNumberOnPage) {
+        hiddenLetters.push(letterBox.lastChild);
+        letterBox.removeChild(letterBox.lastChild);
+    } else {
+        curNumOfLettersOnPage++;
+    }
+    letterBox.insertBefore(letter, letterBox.firstChild);
+}
+
+//document.getElementById('menu__receive').addEventListener('click', newLetter);
 document.getElementById('actions__remove').addEventListener('click', remove);
 document.getElementById('actions__check').addEventListener('change', checkAll);
-//setInterval(newMail, nextDouble(2000, 2000));
+document.getElementById('letter-entry-board__close-icon').addEventListener('click', () => closeEntryBoard());
+
+setTimeout(letterArrival, randRange(minNewLetterTime, maxNewLetterTime));
