3539aecc6b8cdd561b8228ee21796fa82b6c6501
khalimov98 <khalimov98@yandex-team.ru>
2019-04-04 22:57:48 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -1,31 +1,3 @@
-let sentences = [
-    'Первый веб-браузер был создан в 1990 году сэром Тимом Бернерс-Ли',
-    'Он назывался WorldWideWeb и позже был переименован в Nexus',
-    'Но первым распространённым браузером с графическим интерфейсом был NCSA Mosaic',
-    'Исходный код этого одного из первых браузеров был открыт, и некоторые другие браузеры взяли его за основу',
-    'Netscape выпустила Netscape Navigator под разные операционные системы и добилась заметного успеха, в том числе и коммерческого',
-    'Это побудило компанию Microsoft выпустить свой браузер Internet Explorer',
-    'В отличие от Netscape, Microsoft сразу выпускала локализованные версии IE',
-    'В 1995 году Microsoft выпустила операционную систему Windows 95, в ней не было встроенного браузера',
-    'Но через некоторое время в обновление системы (Windows 95 OSR2) он был встроен (Internet Explorer 3)',
-    'К тому же Microsoft добавляла в свой браузер несовместимые со стандартами расширения языка HTML',
-    'Это можно считать началом войны браузеров, закончившейся монополизацией (более 95 %) рынка браузером от Microsoft',
-    'Из-за потери рынка доходы компании Netscape упали, и её приобрела AOL',
-    'Исходный код браузера Netscape был выпущен под свободной лицензией MPL (Mozilla Public License)',
-    'Название «Mozilla» изначально присутствовало в браузере от Netscape и означало сокращение слов Mosaic+killer',
-    'В 1992 году компания Nombas (впоследствии приобретённая Openwave) начала разработку встраиваемого скриптового языка Cmm (Си-минус-минус)',
-    'Главным отличием от Си была работа с памятью',
-    'В новом языке всё управление памятью осуществлялось автоматически: не было необходимости создавать буферы, объявлять переменные, осуществлять преобразование типов',
-    'В остальном языки сильно походили друг на друга: в частности, Cmm поддерживал стандартные функции и операторы Си',
-    'Cmm был переименован в ScriptEase, поскольку исходное название звучало слишком негативно, а упоминание в нём Си «отпугивало» людей',
-    'На основе этого языка был создан проприетарный продукт CEnvi',
-    'В конце ноября 1995 года Nombas разработала версию CEnvi, внедряемую в веб-страницы',
-    'Страницы, которые можно было изменять с помощью скриптового языка, получили название Espresso Pages',
-    'Ни демонстрировали использование скриптового языка для создания игры, проверки пользовательского ввода в формы и создания анимации',
-    'Espresso Pages позиционировались как демоверсия, призванная помочь представить, что случится, если в браузер будет внедрён язык Cmm',
-    'Работали они только в 16-битовом Netscape Navigator под управлением Windows'
-];
-
 let names = [
     'Андре', 'Борислав', 'Герасим', 'Вадим', 'Кирилл', 'Ролан', 'Михаил', 'Генрих', 'Василий', 'Всеволод', 'Владимир',
     'Тимур', 'Аристарх', 'Рудольф', 'Клим', 'Эмин', 'Дамир', 'Махмуд', 'Евдоким', 'Трофим', 'Ерофей', 'Джамал', 'Юхим',
@@ -48,35 +20,33 @@ let letterIdToText = new Map();
 counter = 0;
 let domParser = new DOMParser();
 
+function generateRandomInt(from, to) {
+    return Math.floor(Math.random()*(to - from) + from)
+}
+
 function getRandomElement(array) {
-    return array[Math.floor(Math.random()*array.length)]
+    return array[generateRandomInt(0, array.length)]
 }
 
 letterIds = [];
+maxLetters = 30;
 
-function addNewLetter() {
+async function newMail() {
     function generateDate() {
         let date = new Date();
         return date.getDay() + ' ' + months[date.getMonth()]
     }
 
-    function generateText() {
+    async function generateText() {
+        let paragraphsCount = generateRandomInt(3, 5);
+
+        let text = await fetch("https://baconipsum.com/api/?type=meat&formaat=json&paras=" + paragraphsCount);
+        let paragraphs = await text.json();
         let res = '';
-
-        let paragraphCount = Math.floor(Math.random() * 2 + 2);
-
-        for (let i = 0; i < paragraphCount; i++) {
-            res += '<p>';
-            let sentenceCount = Math.floor(Math.random() * 19 + 1);
-
-            for (let j = 0; j < sentenceCount; j++) {
-                res += getRandomElement(sentences);
-                res += '. ';
-            }
-            res += '</p>'
+        for (let i = 0; i < paragraphsCount; i++) {
+            res += '<p>' + paragraphs[i] + '</p>';
         }
-
-        return res
+        return res;
     }
 
     let name = getRandomElement(names);
@@ -85,7 +55,7 @@ function addNewLetter() {
     let author = name + ' ' + surname;
 
     let id = 'letter-id' + counter++;
-    let text = generateText();
+    let text = await generateText();
     letterIdToText.set(id, text);
     let subject = text.split('.')[0].substr(3);
 
@@ -106,10 +76,6 @@ function addNewLetter() {
                     `;
     let newLetterNode = domParser.parseFromString(newLetterCode, "text/html").body.firstChild;
 
-    letterIds.push(id);
-    if (letterIds.length > 30) {
-        newLetterNode.style.display = "none"
-    }
     newLetterNode.addEventListener("webkitAnimationEnd", function () {
         newLetterNode.classList.remove('letter__animated-add-letter');
     });
@@ -120,10 +86,15 @@ function addNewLetter() {
 
     let letters = document.querySelector(".letters");
     letters.insertBefore(newLetterNode, letters.firstChild);
+
+    letterIds.push(id);
+    if (letterIds.length > maxLetters) {
+        letters.childNodes[maxLetters].style.display = "none"
+    }
 }
 
 let newLetterButton = document.querySelector(".menu__new-letter-button");
-newLetterButton.addEventListener('click', addNewLetter);
+newLetterButton.addEventListener('click', newMail);
 
 function openLetter(id) {
     let openedLetter = document.querySelector("#opened-letter");
@@ -150,7 +121,7 @@ function deleteLetter() {
     let letters = document.querySelectorAll(".letter");
     letters.forEach(letter => {
         if (letter.querySelector(".letter__checkbox").checked) {
-            letter.className += " letter__animated-delete-letter";
+            letter.classList.add("letter__animated-delete-letter");
             letter.addEventListener("webkitAnimationEnd", function () {
                 letter.remove()
             });
@@ -160,7 +131,7 @@ function deleteLetter() {
 
     setTimeout(function () {
         letterIds = letterIds.filter(id => document.getElementById(id) != null);
-        for (let i = 0; i < Math.min(letterIds.length, 30); i++) {
+        for (let i = letterIds.length - 1; i >= Math.max(0, letterIds.length - maxLetters); i--) {
             console.log(document.getElementById(letterIds[i]));
             document.getElementById(letterIds[i]).style.display = "inline-block";
         }
@@ -186,11 +157,11 @@ selectAllCheckbox.addEventListener('click', selectAll);
 let last = 300000;
 
 function recursiveGenerateLetters() {
-    addNewLetter();
+    newMail();
     let fiveMinute = 300000;
     let maxTime = 600000;
     let minTime = 10;
-    let time =  Math.max(fiveMinute - last, Math.floor(Math.random() * (maxTime - minTime) + minTime));
+    let time =  Math.max(fiveMinute - last, generateRandomInt(minTime, maxTime));
     last = time;
     setTimeout(recursiveGenerateLetters, time);
 }
