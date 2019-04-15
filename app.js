eb0d5d37256c4b8885f970e779e1bc4552e0cbba
Nikita Voloshin <nik.volosh@yandex.ru>
2019-04-16 00:15:17 +0300

    Change Server to createServer

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = require('./server/routes');
